bea887da8db22d9c1191c59c9ce1fcc567b87bbb
Kina67 <lugarbugli@libero.it>
2025-08-26 15:42:37 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/Sicilia",
   assetPrefix: "/Sicilia/",
   images: { unoptimized: true },
 };
-
 export default nextConfig;
